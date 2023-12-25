59de6248c56f9931ecf7c1e8991135fdef958b30
Fedor Abrosimov <fedor.abrosimov94@gmail.com>
2023-12-25 12:08:44 +0300

    table check

--- src/test/java/guru/qa/PracticeFormTest.java
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -3,6 +3,7 @@ package guru.qa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import java.io.File;
 
@@ -17,7 +18,7 @@ public class PracticeFormTest {
     Configuration.browserSize = "1920x1080";
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.pageLoadStrategy = "eager";
-    Configuration.holdBrowserOpen = true;
+    Configuration.holdBrowserOpen = false;
     Configuration.timeout = 5000;
   }
   @Test
@@ -28,7 +29,7 @@ public class PracticeFormTest {
     $("#lastName").setValue("lastName");
     $("#userEmail").setValue("userEmail@userEmail.com");
     $("#gender-radio-1").parent().click();
-    $("#userNumber").setValue("79955947011");
+    $("#userNumber").setValue("9955947011");
     $("#dateOfBirthInput").click();
     $(".react-datepicker__month-select").selectOption(3);
     $(".react-datepicker__year-select").selectOption(94);
@@ -46,6 +47,15 @@ public class PracticeFormTest {
     
     //check
     $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    $(By.tagName("tbody")).shouldHave(text("firstName"));
+    $(By.tagName("tbody")).shouldHave(text("lastName"));
+    $(By.tagName("tbody")).shouldHave(text("9955947011"));
+    $(By.tagName("tbody")).shouldHave(text("21 April,1994"));
+    $(By.tagName("tbody")).shouldHave(text("Maths"));
+    $(By.tagName("tbody")).shouldHave(text("Sports"));
+    $(By.tagName("tbody")).shouldHave(text("Screenshot at Aug 28 13-08-24.png"));
+    $(By.tagName("tbody")).shouldHave(text("currentAddress"));
+    $(By.tagName("tbody")).shouldHave(text("Uttar Pradesh Merrut"));
     
   }
 }
