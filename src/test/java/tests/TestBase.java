997821fbcf8c497f94e9648a6ce910a320b79e3b
Fedor Abrosimov <fedor.abrosimov94@gmail.com>
2024-03-12 22:55:29 +0300

    remote configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,6 +14,8 @@ public class TestBase {
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.pageLoadStrategy = "eager";
     SelenideLogger.addListener("allure", new AllureSelenide());
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    
     
     
   }
