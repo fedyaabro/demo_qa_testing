d6b71ba0283f738e06af8219eec9c22ec51e2953
Fedor Abrosimov <fedor.abrosimov94@gmail.com>
2024-03-22 13:00:52 +0300

    new debug with fork 6

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -18,9 +19,6 @@ public class TestBase {
     
     SelenideLogger.addListener("allure", new AllureSelenide());
     
-    System.setProperty("environment", System.getProperty("environment", "prod"));
-    
-//    Configuration.browserSize = "1920x1080";
     Configuration.remote = System.getProperty("browserRemoteUrl");
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.pageLoadStrategy = "eager";
@@ -28,14 +26,15 @@ public class TestBase {
     Configuration.browserVersion = System.getProperty("browserVersion", "113.0");
     Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
     
-//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("selenoid:options", Map.<String, Object>of(
       "enableVNC", true,
       "enableVideo", true));
+    
     Configuration.browserCapabilities = capabilities;
   }
+  
   
   @AfterEach
   void addAttachments() {
