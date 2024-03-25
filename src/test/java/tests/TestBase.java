5ba43c26d0587267a163b973d13e930941d1461c
Fedor Abrosimov <fedor.abrosimov94@gmail.com>
2024-03-25 17:00:45 +0300

    final debug

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,7 +21,10 @@ public class TestBase {
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.pageLoadStrategy = "eager";
     SelenideLogger.addListener("allure", new AllureSelenide());
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    Configuration.remote = System.getProperty("browserRemoteUrl");
+    
+    
     
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("selenoid:options", Map.<String, Object>of(
